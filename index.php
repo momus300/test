9f3446ab7f7e74651d1dfff616f42f065ed69ca1
momus <momus300@gmail.com>
2015-09-03 23:38:53 +0200

    moj pierwszy push :)

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$options = [
+    'location' => 'http://localhost/test/serverSoap.php',
+    'uri' => 'http://localhost'
+];
+//create an instante of the SOAPClient (the API will be available)
+$api = new SoapClient(NULL, $options);
+//call an API method
+echo $api->hello();
+echo $api->dzialanie([3, 2]);
---
+++ serverSoap.php
@@ -0,0 +1,25 @@
+<?php
+
+class MyAPI
+{
+    function hello()
+    {
+        return "Hello";
+    }
+
+    function dzialanie($a)
+    {
+        return implode('.', $a);
+    }
+}
+
+//when in non-wsdl mode the uri option must be specified
+$options = array('uri' => 'http://localhost');
+//create a new SOAP server
+$server = new SoapServer(NULL, $options);
+//attach the API class to the SOAP Server
+$server->setClass('MyAPI');
+
+//start the SOAP requests handler
+$server->handle();
+//$server->dzialanie($a);
